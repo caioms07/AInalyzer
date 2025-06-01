0cc016898d63aab77cd892a38ffa44e6a85e0af3
caiosousa <caiomsousa@outlook.pt>
2025-06-02 00:32:21 +0100

    Project init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# java-ai
+# AInalyzer
+
+This project
---
+++ ainalyzer-application/src/test/java/com/example/ainalyzer/application/AinalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ainalyzer.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AinalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
